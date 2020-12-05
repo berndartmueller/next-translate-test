9d3fa3d329c928847189b670d5f7d709d45a8e6d
Bernd Artmüller <berndartmueller@gmail.com>
2020-12-05 19:19:47 +0100

    add custom document

--- _pages/_app.js
+++ _pages/_app.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
+import Head from 'next/head';
 
 function App({ Component, pageProps }) {
 
   return (
     <Fragment>
+        <Head></Head>
         <Component {...pageProps} />
     </Fragment>
   );
